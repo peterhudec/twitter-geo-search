1a7537e7a5eb5bab7ff0a009d331e8731a208111
Peter Hudec <peterhudec@peterhudec.com>
2015-09-20 18:44:52 +0200

    Added map rudiments.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,13 +1,22 @@
-var tweetSearchApp = angular.module('tweetSearchApp', []);
+var tweetSearchApp = angular.module('tweetSearchApp', ["leaflet-directive"]);
 
 tweetSearchApp.controller('TweetListController', function ($scope, $http) {
+	angular.extend($scope, {
+      center: {
+          lat: 40.095,
+          lng: -3.823,
+          zoom: 4
+      },
+      defaults: {
+          scrollWheelZoom: false
+      }
+  });
 
 	$scope.geoFilter = function (item) {
 		return !$scope.showGeo || item.coordinates !== null;
 	};
 
 	$scope.search = function () {
-
 		console.log($scope.query);
 
 		$http.get('search', {
@@ -25,4 +34,22 @@ tweetSearchApp.controller('TweetListController', function ($scope, $http) {
 				console.log(err);
 			});
 	};
+});
+
+tweetSearchApp.controller('GeoItemController', function ($scope) {
+	var coordinates = $scope.$parent.tweet.coordinates.coordinates;
+
+	angular.extend($scope, {
+		center: {
+			zoom: 14,
+			lng: coordinates[0],
+			lat: coordinates[1]
+		},
+		markers: {
+			marker: {
+				lng: coordinates[0],
+				lat: coordinates[1]
+			}
+		}
+	});
 });
